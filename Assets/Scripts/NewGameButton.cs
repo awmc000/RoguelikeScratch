a4b88a24b607eea8735a94e284b4fa3cf703b356
awmc000 <mccolmalex@gmail.com>
2023-06-23 12:43:53 -0700

    Added new game button to repo

---
+++ Assets/Scripts/NewGameButton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGameButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnPress()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+}
