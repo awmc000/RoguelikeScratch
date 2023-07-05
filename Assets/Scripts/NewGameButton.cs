6d4b66078af9fe3674490a570b6b2e13ed282c84
awmc000 <mccolmalex@gmail.com>
2023-07-05 15:05:08 -0700

    Settings button hides settings

--- Assets/Scripts/NewGameButton.cs
+++ Assets/Scripts/NewGameButton.cs
@@ -14,11 +14,19 @@ public class NewGameButton : MonoBehaviour
         Application.Quit();
     }
 
-    public bool settingsOpen = false;
+    public bool settingsOpen = true;
 
     public void OnToggleSettings()
     {
         settingsOpen = !settingsOpen;
         GameObject resolutionButtons = GameObject.Find("ResolutionButtons");
+        if (settingsOpen)
+        {
+            resolutionButtons.transform.position -= new Vector3(1000, 1000, 0);
+        }
+        else
+        {
+            resolutionButtons.transform.position += new Vector3(1000, 1000, 0);
+        }
     }
 }
